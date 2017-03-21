8290ed4343f8161a8e64ce8878e031b9e8b5761c
Yuliy Ayvazyan <yuliy@yuliy.me>
2017-03-22 01:46:32 +0300

    @

---
+++ yuliy/discover_phaser/first-game/main.js
@@ -0,0 +1,18 @@
+var mainState = {
+    preload: function() {
+        //
+    },
+
+    create: function() {
+        game.stage.backgroundColor = '#3498db';
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+    },
+
+    update: function() {
+        //
+    }
+};
+
+var game = new Phaser.Game(500, 340, Phaser.AUTO, 'gameDiv');
+game.state.add('main', mainState);
+game.state.start('main');
