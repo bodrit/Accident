123f6067ecadc0fb3846b20e84118e7371360ff2
Yuliy Ayvazyan <yuliy@yuliy.me>
2017-03-24 02:15:19 +0300

    Added walls and collisions

--- yuliy/discover_phaser/first-game/main.js
+++ yuliy/discover_phaser/first-game/main.js
@@ -1,13 +1,17 @@
 var mainState = {
     preload: function() {
         game.load.image('player', 'assets/player.png');
+        game.load.image('wallV', 'assets/wallVertical.png');
+        game.load.image('wallH', 'assets/wallHorizontal.png');
     },
 
     create: function() {
+        //
         game.stage.backgroundColor = '#3498db';
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.renderer.renderSession.roundPixels = true;
 
+        // player
         this.player = game.add.sprite(game.width/2, game.height/2, 'player');
         this.player.anchor.setTo(0.5, 0.5);
 
@@ -15,10 +19,18 @@ var mainState = {
         this.player.body.gravity.y = 500;
 
         this.cursor = game.input.keyboard.createCursorKeys();
+
+        // world
+        this.createWorld();
     },
 
     update: function() {
+        game.physics.arcade.collide(this.player, this.walls);
         this.movePlayer();
+
+        if (!this.player.inWorld) {
+            this.playerDie();
+        }
     },
 
     movePlayer: function() {
@@ -33,6 +45,33 @@ var mainState = {
         if (this.cursor.up.isDown && this.player.body.touching.down) {
             this.player.body.velocity.y = -320;
         }
+    },
+
+    createWorld: function() {
+        this.walls = game.add.group();
+        this.walls.enableBody = true;
+
+        game.add.sprite(0, 0, 'wallV', 0, this.walls);
+        game.add.sprite(480, 0, 'wallV', 0, this.walls);
+
+        game.add.sprite(0, 0, 'wallH', 0, this.walls);
+        game.add.sprite(300, 0, 'wallH', 0, this.walls);
+        game.add.sprite(0, 320, 'wallH', 0, this.walls);
+        game.add.sprite(300, 320, 'wallH', 0, this.walls);
+
+        game.add.sprite(-100, 160, 'wallH', 0, this.walls);
+        game.add.sprite(400, 160, 'wallH', 0, this.walls);
+
+        var middleTop = game.add.sprite(100, 80, 'wallH', 0, this.walls);
+        middleTop.scale.setTo(1.5, 1);
+        var middleBottom = game.add.sprite(100, 240, 'wallH', 0, this.walls);
+        middleBottom.scale.setTo(1.5, 1);
+
+        this.walls.setAll('body.immovable', true);
+    },
+
+    playerDie: function() {
+        game.state.start('main');
     }
 };
 
