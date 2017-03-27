ad21d06071e2be4d07bca09232b43d439371a0fa
Yuliy Ayvazyan <yuliy@yuliy.me>
2017-03-28 02:05:09 +0300

    Removed commented code

--- yuliy/discover_phaser/first-game/js/play.js
+++ yuliy/discover_phaser/first-game/js/play.js
@@ -138,7 +138,3 @@ var playState = {
         enemy.outOfBoundsKill = true;
     }
 };
-
-//var game = new Phaser.Game(500, 340, Phaser.AUTO, 'gameDiv');
-//game.state.add('play', playState);
-//game.state.start('play');
