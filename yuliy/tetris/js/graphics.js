5e24aff63c486de5e2f4ef677a28c53af41bb946
Yuliy Ayvazyan <yuliy@yuliy.me>
2017-04-12 00:55:07 +0300

    Moved some GraphicsEngine fields to private

--- yuliy/tetris/js/graphics.js
+++ yuliy/tetris/js/graphics.js
@@ -4,40 +4,40 @@ function GraphicsEngine(
     fieldSize,
     fieldPosition
 ) {
-    this.tileSize = {x: 20, y: 20};
-    this.fieldSize = {x: 10, y: 20};
-    this.fieldPosition = {x: 50, y: 50};
+    this._tileSize = {x: 20, y: 20};
+    this._fieldSize = {x: 10, y: 20};
+    this._fieldPosition = {x: 50, y: 50};
 
     this.onPreload = function() {
         app.game.load.spritesheet(
             'brick', 'assets/brick.png',
-            this.tileSize.x, this.tileSize.y
+            this._tileSize.x, this._tileSize.y
         );
     };
 
     this.onCreate = function() {
-        this.field = [];
+        this._field = [];
 
-        for (var i = 0; i != this.fieldSize.y; ++i) {
+        for (var i = 0; i != this._fieldSize.y; ++i) {
             var line = [];
-            for (var j = 0; j != this.fieldSize.x; ++j) {
+            for (var j = 0; j != this._fieldSize.x; ++j) {
                 var cell = app.game.add.sprite(
-                    this.fieldPosition.x + this.tileSize.x * j,
-                    this.fieldPosition.y + this.tileSize.y * i,
+                    this._fieldPosition.x + this._tileSize.x * j,
+                    this._fieldPosition.y + this._tileSize.y * i,
                     'brick'
                 );
                 cell.frame = 0;
                 line.push(cell);
             }
-            this.field.push(line);
+            this._field.push(line);
         }
     }
 
     this.updateField = function(fieldData) {
-        for (var i = 0; i != this.fieldSize.y; ++i) {
-            for (var j = 0; j != this.fieldSize.x; ++j) {
-                this.field[i][j].frame = fieldContent[i][j];
-                //this.field[i][j].frame = app.game.rnd.pick([0, 1]);
+        for (var i = 0; i != this._fieldSize.y; ++i) {
+            for (var j = 0; j != this._fieldSize.x; ++j) {
+                //this._field[i][j].frame = fieldData[i][j];
+                this._field[i][j].frame = app.game.rnd.pick([0, 1]);
             }
         }
     }
---
+++ yuliy/tetris/js/physics.js
@@ -0,0 +1,3 @@
+
+function PhysicsEngine(graphicsEngine) {
+}
